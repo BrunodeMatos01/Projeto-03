e7522901c028e4762cedc9ff5dbdef10a3cb3d15
Bruno Matos <brunodematos3@gmail.com>
2025-04-22 21:43:55 -0300

    Criando components de nav em components e login e login-vendedores em pages

---
+++ Projeto3/FrontEnd/AngularProjeto3/src/app/pages/login-vendedores/login-vendedores.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-vendedores',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-vendedores.component.html',
+  styleUrl: './login-vendedores.component.css'
+})
+export class LoginVendedoresComponent {
+
+}
