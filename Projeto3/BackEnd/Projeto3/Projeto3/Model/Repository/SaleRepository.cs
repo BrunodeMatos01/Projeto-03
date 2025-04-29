f254219d25b01dfa40ccee98c26306dedc0fc07a
LucasS-alvador <lucassalvador948@gmail.com>
2025-04-29 03:39:07 -0300

    Easy demais só demorou 3 horas além do esperado para fazer a migratios. 03:39

---
+++ Projeto3/BackEnd/Projeto3/Projeto3/Model/Repository/SaleRepository.cs
@@ -0,0 +1,56 @@
+﻿using C_Projeto3.Infra.Data;
+using C_Projeto3.Model.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace C_Projeto3.Model.Repository
+{
+    public class SaleRepository : ISaleRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public SaleRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<bool> Delete(Guid id)
+        {
+            try
+            {
+                var saleToRemove = _appDbContext.sales.FirstOrDefault(a => a.id == id);
+                if (saleToRemove != null)
+                {
+                    _appDbContext.sales.Remove(saleToRemove);
+                    await _appDbContext.SaveChangesAsync();
+                    return true;
+                }
+                throw new Exception("Sale not found");
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error when removing sale");
+            }
+        }
+
+        public Task Edit(Sale sale)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Sale>> List()
+        {
+            return await _appDbContext.sales.ToListAsync();
+        }
+
+        public async Task Save(Sale sale)
+        {
+            _appDbContext.sales.Add(sale);
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task<Sale> SearchById(Guid id)
+        {
+            return await _appDbContext.sales.FindAsync(id);
+        }
+    }
+}
---
+++ Projeto3/BackEnd/Projeto3/Projeto3/Model/product_sale.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace C_Projeto3.Model
+{
+    public class product_sale
+    {
+        [Key]
+        public Guid id { get; set; }
+
+        [Required]
+        public Guid sale_id { get; set; }
+
+        [Required]
+        public Guid product_id { get; set; }
+
+        [Required]
+        public int quantity { get; set; }
+    }
+}
